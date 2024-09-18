6a6741aa0c7f09230d7f65d8196a7c35befae61c
Laslo Hauschild <dev@laslo-hauschild.eu>
2024-09-18 21:28:29 +0200

    Rewrote transactions

--- lib/providers/transactions.dart
+++ lib/providers/transactions.dart
@@ -1,235 +1,158 @@
-import 'dart:collection';
 import 'dart:io';
 
 import 'package:csv/csv.dart';
 import 'package:csv/csv_settings_autodetection.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class Transaction {
-  String date;
-  String id;
-  int number;
-  String description;
-  String notes;
-  String commodityCurrency;
-  String voidReason;
-  String action;
-  String memo;
-  String fullAccountName;
-  String accountName;
-  String amountWithSymbol;
-  double amount;
-  String reconcile = "n";
-  String reconcileDate;
-  int ratePrice;
+part 'transactions.freezed.dart';
+part 'transactions.g.dart';
 
-  Transaction();
-
-  Transaction.fromList(List<dynamic> items) {
-    final trimmed = [];
-    for (var item in items) trimmed.add(item.trim());
-
-    this.date = trimmed[0] ?? "";
-    this.id = trimmed[1];
-    this.number = int.tryParse(trimmed[2]) ?? null;
-    this.description = trimmed[3];
-    this.notes = trimmed[4];
-    this.commodityCurrency = trimmed[5];
-    this.voidReason = trimmed[6];
-    this.action = trimmed[7];
-    this.memo = trimmed[8];
-    this.fullAccountName = trimmed[9];
-    this.accountName = trimmed[10];
-    this.amountWithSymbol = trimmed[11];
-    this.amount = double.tryParse(trimmed[12]) ?? null;
-    this.reconcile = trimmed[13];
-    this.reconcileDate = trimmed[14];
-    this.ratePrice = int.tryParse(trimmed[15]) ?? null;
+late String _transactionCSV;
+/// TODO: Initialize in main.dart
+Future<void> initAccounts() async {
+  /// TODO: Using applicationSupportDirectory is discouraged for userData, migrate to SharedPreferences
+  Directory directory = await getApplicationSupportDirectory();
+  File file = File('${directory.path}/transactions.csv');
+  if (!file.existsSync()) {
+    _transactionCSV = "";
+    return;
   }
+  _transactionCSV = await file.readAsString();
+}
 
-  @override
-  toString() {
-    return """Transaction{date: ${this.date}, id: ${this.id}, number: ${this.number}, description: ${this.description}, notes: ${this.notes}, commodityCurrency: ${this.commodityCurrency}, voidReason: ${this.voidReason}, action: ${this.action}, memo: ${this.memo}, fullAccountName: ${this.fullAccountName}, accountName: ${this.accountName}, amountWithSymbol: ${this.amountWithSymbol}, amount: ${this.amount}, reconcile: ${this.reconcile}, reconcileDate: ${this.reconcileDate}, ratePrice: ${this.ratePrice}}""";
+@freezed
+class Transaction with _$Transaction {
+  const factory Transaction({
+    required String date,
+    required String id,
+    required int? number,
+    required String description,
+    required String notes,
+    required String commodityCurrency,
+    required String voidReason,
+    required String action,
+    required String memo,
+    required String fullAccountName,
+    required String accountName,
+    required String amountWithSymbol,
+    required double? amount,
+    required String reconcile,
+    required String reconcileDate,
+    required int? ratePrice,
+  }) = _Transaction;
+  const Transaction._();
+
+  factory Transaction.fromCSVLine(List<String> items) {
+    items = items.map((item) => item.trim()).toList();
+
+    return Transaction(
+      date: items[0] ?? "",
+      id: items[1],
+      number: int.tryParse(items[2]) ?? null,
+      description: items[3],
+      notes: items[4],
+      commodityCurrency: items[5],
+      voidReason: items[6],
+      action: items[7],
+      memo: items[8],
+      fullAccountName: items[9],
+      accountName: items[10],
+      amountWithSymbol: items[11],
+      amount: double.tryParse(items[12]) ?? null,
+      reconcile: items[13],
+      reconcileDate: items[14],
+      ratePrice: int.tryParse(items[15]) ?? null,
+    );
   }
-
-  List<dynamic> toList() {
+  List<dynamic> toCSVLine() {
     return [
-      this.date ?? "",
-      this.id ?? "",
-      this.number ?? "",
-      this.description ?? "",
-      this.notes ?? "",
-      this.commodityCurrency ?? "",
-      this.voidReason ?? "",
-      this.action ?? "",
-      this.memo ?? "",
-      this.fullAccountName ?? "",
-      this.accountName ?? "",
-      this.amountWithSymbol ?? "",
-      this.amount ?? "",
-      this.reconcile ?? "",
-      this.reconcileDate ?? "",
-      this.ratePrice ?? ""
+      date,
+      id,
+      number ?? "",
+      description,
+      notes,
+      commodityCurrency,
+      voidReason,
+      action,
+      memo,
+      fullAccountName,
+      accountName,
+      amountWithSymbol,
+      amount ?? "",
+      reconcile,
+      reconcileDate,
+      ratePrice ?? "",
     ];
   }
+
+  factory Transaction.fromJson(Map<String, dynamic> json) => _$TransactionFromJson(json);
 }
 
-class TransactionsModel extends ChangeNotifier {
-  Future<String> get _localPath async {
-    final directory = await getApplicationSupportDirectory();
-    return directory.path;
+@riverpod
+class Transactions extends _$Transactions {
+  @override
+  List<Transaction> build() {
+    return _getCachedTransactions();
   }
 
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/transactions.csv');
+  List<Transaction> _getCachedTransactions() {
+    // Using _accountCSV.
+    // TODO: Migrate data collection to SharedPreferences
+
+    // Convert CSV to List<List<String>>
+    const detector = FirstOccurrenceSettingsDetector(
+      eols: ['\r\n', '\n'],
+    );
+    const converter = CsvToListConverter(
+      csvSettingsDetector: detector,
+      textDelimiter: '"',
+      shouldParseNumbers: false,
+    );
+
+    List<List<String>> parsedCSV = converter.convert(_transactionCSV.trim());
+
+    // Convert List<List<String>> to List<Transaction>
+    return List.unmodifiable(parsedCSV.map((line) => Transaction.fromCSVLine(line)).toList());
+  }
+  void _setCachedTransactions() async {
+    Directory directory = await getApplicationSupportDirectory();
+    File file = File('${directory.path}/transactions.csv');
+    String csvString = const ListToCsvConverter(eol: "\n")
+      .convert(state.map((transaction) => transaction.toCSVLine()).toList());
+    await file.writeAsString(csvString);
   }
 
-  Future<String> readTransactionsCsv() async {
-    try {
-      final _file = await _localFile;
-      final _string = await _file.readAsString();
-      return "Date,Transaction ID,Number,Description,Notes,Commodity/Currency,Void Reason,Action,Memo,Full Account Name,Account Name,Amount With Sym.,Amount Num,Reconcile,Reconcile Date,Rate/Price\n$_string";
-    } catch (e) {
-      print("readTransactionsCsv error");
-      print(e);
-      return null;
-    }
+  void addAll(List<Transaction> transactions) {
+    state = List.unmodifiable([...state, ...transactions]);
+    _setCachedTransactions();
   }
-
-  Map<String, List<Transaction>> _transactionsByAccountFullName = Map();
-
-  UnmodifiableMapView<String, List<Transaction>>
-      get transactionsByAccountFullName {
-    return UnmodifiableMapView(_transactionsByAccountFullName);
+  void removeAll() {
+    state = List.unmodifiable([]);
+    _setCachedTransactions();
   }
-
-  Future<UnmodifiableListView<Transaction>> get transactions async {
-    try {
-      final file = await _localFile;
-      String contents = await file.readAsString();
-
-      var _detector = new FirstOccurrenceSettingsDetector(
-        eols: ['\r\n', '\n'],
-      );
-
-      final _converter = CsvToListConverter(
-        csvSettingsDetector: _detector,
-        textDelimiter: '"',
-        shouldParseNumbers: false,
-      );
-
-      final _parsed = _converter.convert(contents.trim());
-
-      final _transactions = <Transaction>[];
-      final Map<String, List<Transaction>> _transactionsByAccountFullName =
-          Map();
-      for (var line in _parsed) {
-        final _transaction = Transaction.fromList(line);
-        _transactions.add(_transaction);
-
-        // Add to representation of balances
-        if (_transactionsByAccountFullName
-            .containsKey(_transaction.fullAccountName)) {
-          _transactionsByAccountFullName[_transaction.fullAccountName]
-              .add(_transaction);
-        } else {
-          _transactionsByAccountFullName[_transaction.fullAccountName] = [
-            _transaction
-          ];
-        }
-      }
-
-      this._transactionsByAccountFullName = _transactionsByAccountFullName;
-
-      return UnmodifiableListView(_transactions);
-    } catch (e) {
-      print("readTransactions error");
-      print(e);
-      return null;
-    }
+  void remove(Transaction transaction) {
+    state = List.unmodifiable([...state]..remove(transaction));
+    _setCachedTransactions();
   }
-
-  void addAll(List<Transaction> transactions) async {
-    final file = await _localFile;
-    final _csvString = const ListToCsvConverter(eol: "\n")
-        .convert(transactions.map((t) => t.toList()).toList());
-
-    try {
-      file.writeAsString(
-        "$_csvString\n",
-        mode: FileMode.append,
-      );
-    } catch (e) {
-      print("addAll error");
-      print(e);
-    }
-
-    // Add to representation of balances
-    for (var _transaction in transactions) {
-      if (_transactionsByAccountFullName
-          .containsKey(_transaction.fullAccountName)) {
-        _transactionsByAccountFullName[_transaction.fullAccountName]
-            .add(_transaction);
-      } else {
-        _transactionsByAccountFullName[_transaction.fullAccountName] = [
-          _transaction
-        ];
-      }
-    }
-
-    notifyListeners();
-  }
-
-  void removeAll() async {
-    final file = await _localFile;
-
-    try {
-      file.delete();
-    } catch (e) {
-      print(e);
-    }
-
-    _transactionsByAccountFullName.clear();
-
-    notifyListeners();
-  }
-
-  Future<bool> remove(Transaction transaction) async {
-    try {
-      final file = await _localFile;
-      final lines = await file.readAsLines();
-
-      final toRemove = [];
-      for (var line in lines) {
-        if (line.contains(transaction.id)) {
-          toRemove.add(line);
-        }
-      }
-
-      if (toRemove.length == 0) {
-        return false;
-      }
-
-      for (var line in toRemove) {
-        lines.remove(line);
-      }
-
-      file.writeAsString(lines.toString());
-
-      // TODO: Remove this hack attack-y way of refreshing the transactions state
-      _transactionsByAccountFullName.clear();
-      await transactions;
-
-      notifyListeners();
-
-      return true;
-    } catch (err) {
-      print("removeTransaction error");
-      print(err);
-
-      return false;
-    }
+  String getCSV() {
+    String content = const ListToCsvConverter(eol: "\n")
+      .convert(state.map((transaction) => transaction.toCSVLine()).toList());
+    return "Date,Transaction ID,Number,Description,Notes,Commodity/Currency,Void Reason,Action,Memo,Full Account Name,Account Name,Amount With Sym.,Amount Num,Reconcile,Reconcile Date,Rate/Price\n$content";
   }
 }
+
+@riverpod
+Map<String, List<Transaction>> transactionsByAccountFullName(TransactionsByAccountFullNameRef ref) {
+  final transactions = ref.watch(transactionsProvider);
+  final Map<String, List<Transaction>> transactionsByAccountFullName = {};
+
+  for (var transaction in transactions) {
+    transactionsByAccountFullName.putIfAbsent(transaction.fullAccountName, () => []);
+    transactionsByAccountFullName[transaction.fullAccountName]!.add(transaction);
+  }
+  return transactionsByAccountFullName;
+}
+
