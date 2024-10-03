544332ef653e2c3c589856661497309b5cb62579
Laslo Hauschild <dev@laslo-hauschild.eu>
2024-10-03 20:32:21 +0200

    changed transaction model

--- lib/core/models/transaction.dart
+++ lib/core/models/transaction.dart
@@ -2,52 +2,53 @@ import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:gnucash_mobile/core/models/account.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 part 'transaction.freezed.dart';
 part 'transaction.g.dart';
 
+/// A part of a transaction.
+///
+/// A transaction contains at least two parts.
+/// The sum of the amounts of all parts must be zero.
 @freezed
-class SingleTransaction with _$SingleTransaction implements Comparable<SingleTransaction> {
-  const factory SingleTransaction({
-    /// The date of the transaction.
-    required DateTime date,
+class TransactionPart with _$TransactionPart {
+  const factory TransactionPart({
+    required Account account,
+    required double amount,
+  }) = _TransactionPart;
 
-    /// The identifier of the transaction.
-    ///
-    /// This identifier is used for both entries in the double-entry system.
-    required String id,
-    required String description,
-    required String notes,
+  const TransactionPart._();
+  factory TransactionPart.fromJson(Map<String, dynamic> json) => _$TransactionPartFromJson(json);
+}
+
+@freezed
+class Transaction with _$Transaction implements Comparable<Transaction> {
+  @Assert('parts.length >= 2', 'A transaction must have at least two parts')
+  @Assert('parts.map((part) => part.amount).reduce((a, b) => a + b) == 0', 'The sum of all parts must be zero')
+  const factory Transaction({
+    required List<TransactionPart> parts,
 
     /// The type of commodity and currency used in the transaction.
     ///
     /// Example: `CURRENCY::EUR`, `CURRENCY::USD`, `CURRENCY::GBP`
     required String commodityCurrency,
-    required double amount,
-  }) = _SingleTransaction;
+    required DateTime date,
+    required String description,
+    @Default("") String notes,
+  }) = _Transaction;
 
-  const SingleTransaction._();
+  const Transaction._();
 
-  static SingleTransaction empty() {
-    return SingleTransaction(
-      date: DateTime.now(),
-      id: "",
-      description: "",
-      notes: "",
-      commodityCurrency: "",
-      amount: 0.0,
-    );
-  }
-
-  factory SingleTransaction.fromJson(Map<String, dynamic> json) => _$SingleTransactionFromJson(json);
+  factory Transaction.fromJson(Map<String, dynamic> json) => _$TransactionFromJson(json);
 
   @override
-  int compareTo(SingleTransaction other) {
+  int compareTo(Transaction other) {
     int dateCompare = date.compareTo(other.date);
     if (dateCompare != 0) {
       return dateCompare;
     }
-    return amount.compareTo(other.amount);
+    return description.compareTo(other.description);
   }
 }
 
@@ -55,10 +56,15 @@ class SingleTransaction with _$SingleTransaction implements Comparable<SingleTra
 ///
 /// The rows are formatted as follows:
 /// Date,Transaction ID,Number,Description,Notes,Commodity/Currency,Void Reason,Action,Memo,Full Account Name,Account Name,Amount With Sym,Amount Num.,Value With Sym,Value Num.,Reconcile,Reconcile Date,Rate/Price
-List<String> transactionToCSV(Account account, SingleTransaction transaction) {
+List<List<String>> transactionToCSV(Transaction transaction) {
+  String id = const Uuid().v4();
+  return transaction.parts.map((part) => _transactionPartToCSV(transaction, part, id)).toList();
+}
+
+List<String> _transactionPartToCSV(Transaction transaction, TransactionPart part, String id) {
   return [
     DateFormat('yyyy-MM-dd').format(transaction.date), // Date
-    transaction.id, // Transaction ID
+    id, // Transaction ID
     "", // Number
     transaction.description, // Description
     transaction.notes, // Notes
@@ -66,24 +72,14 @@ List<String> transactionToCSV(Account account, SingleTransaction transaction) {
     "", // Void Reason
     "", // Action
     "", // Memo
-    account.fullName, // Full Account Name
-    account.name, // Account Name
-    "${transaction.amount} ${account.symbol}", // Amount With Sym
-    "${transaction.amount}", // Amount Num.
-    "${transaction.amount} ${account.symbol}", // Value With Sym
-    "${transaction.amount}", // Value Num.
+    part.account.fullName, // Full Account Name
+    part.account.name, // Account Name
+    "${part.amount} ${part.account.symbol}", // Amount With Sym
+    "${part.amount}", // Amount Num.
+    "${part.amount} ${part.account.symbol}", // Value With Sym
+    "${part.amount}", // Value Num.
     "", // Reconcile
     "", // Reconcile Date
     "", // Rate/Price
   ];
-}
-
-@freezed
-class Transaction with _$Transaction {
-  factory Transaction({
-    required SingleTransaction singleTransaction,
-    required Account account,
-    required SingleTransaction otherSingleTransaction,
-    required Account otherAccount,
-  }) = _Transaction;
 }
